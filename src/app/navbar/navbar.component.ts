97a989572123de59757567942bcf751ff2a3f0a6
dikshya2002 <ddikshya.agarwal2002@gmail.com>
2024-06-23 10:55:22 +0530

    [added button click events]

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -6,6 +6,28 @@ import { Component } from "@angular/core";
 })
 export class NavbarComponent
 {
-    
+    objArray : Array<any> =
+    [
+        {id:1 , ptitle:"post 1 "},
+        {id:2 , ptitle:"post 2 "},
+        {id:3 , ptitle:"post 3 "}
+    ];
+
+    //detect the changes and render it in reeal time 
+
+    addNew()
+    {
+        this.objArray.push(
+            {
+                id:6,ptitle:"newly added post"
+            }
+        )
+    }
+    delete(param:any)
+    {
+        // let i = this.objArray.indexOf(param); we can pass the index directly rather than passing the whole object
+         this.objArray.splice(param,1);
+
+    }
 
 }
